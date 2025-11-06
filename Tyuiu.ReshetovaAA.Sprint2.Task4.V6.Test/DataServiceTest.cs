9282f6f0538f3c5120e0c2f8636a118f9ef55a5a
reshetovaarina <tyuiu.reshetovaaa@outlook.com>
2025-11-06 20:33:07 +0500

    Task4

---
+++ Tyuiu.ReshetovaAA.Sprint2.Task4.V6.Test/DataServiceTest.cs
@@ -0,0 +1,53 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Tyuiu.ReshetovaAA.Sprint2.Task4.V6.Lib;
+
+namespace Tyuiu.ReshetovaAA.Sprint2.Task3.V6.Test
+{
+    [TestClass]
+    public class DataServiceTest
+    {
+        [TestMethod]
+        public void ValidCalculate1()
+        {
+            DataService ds = new DataService();
+            double x = 10;
+            double y = 5;
+            double wait = 0.010; 
+            double res = ds.Calculate(x, y);
+            Assert.AreEqual(wait, res);
+        }
+
+        [TestMethod]
+        public void ValidCalculate2()
+        {
+            DataService ds = new DataService();
+            double x = 5;
+            double y = 20;
+            double wait = 411.200; 
+            double res = ds.Calculate(x, y);
+            Assert.AreEqual(wait, res);
+        }
+
+        [TestMethod]
+        public void ValidCalculate3()
+        {
+            DataService ds = new DataService();
+            double x = 15;
+            double y = 10;
+            double wait = 0.022;
+            double res = ds.Calculate(x, y);
+            Assert.AreEqual(wait, res);
+        }
+
+        [TestMethod]
+        public void ValidCalculate4()
+        {
+            DataService ds = new DataService();
+            double x = 1;
+            double y = 10;
+            double wait = 3.667; 
+            double res = ds.Calculate(x, y);
+            Assert.AreEqual(wait, res);
+        }
+    }
+}
---
+++ Tyuiu.ReshetovaAA.Sprint2.Task4.V6/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using Tyuiu.ReshetovaAA.Sprint2.Task4.V6.Lib;
+
+namespace Tyuiu.ReshetovaAA.Sprint2.Task3.V6
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            DataService ds = new DataService();
+
+            Console.Title = "Спринт #2 | Выполнила: Решетова А. А. | СМАРТб-25-1";
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* Спринт #2                                                               *");
+            Console.WriteLine("* Тема: Тернарный оператор                                                *");
+            Console.WriteLine("* Задание #4                                                              *");
+            Console.WriteLine("* Вариант #6                                                              *");
+            Console.WriteLine("* Выполнила: Решетова Арина Алексеевна | СМАРТб-25-1                      *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* УСЛОВИЕ:                                                                *");
+            Console.WriteLine("* Написать программу, которая вычисляет требуемое значение с              *");
+            Console.WriteLine("* использованием тернарного оператора, где пользователь вводит значение   *");
+            Console.WriteLine("* переменных x, y с клавиатуры.                                           *");
+            Console.WriteLine("* Если x + 20 > y * 2, то z = x * ((y + 1) / (x + 2)) ^ x                 *");
+            Console.WriteLine("* иначе z = y^2 + 2x + (6 / x)                                            *");
+            Console.WriteLine("* Ответ округлите до 3 знаков после запятой.                              *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
+            Console.WriteLine("***************************************************************************");
+
+            Console.Write("Введите значение X: ");
+            double x = Convert.ToDouble(Console.ReadLine());
+
+            Console.Write("Введите значение Y: ");
+            double y = Convert.ToDouble(Console.ReadLine());
+
+            double result = ds.Calculate(x, y);
+
+            Console.WriteLine();
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine($"Z = {result}");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* Программа завершена. Нажмите любую клавишу для выхода.                 *");
+            Console.WriteLine("***************************************************************************");
+
+            Console.ReadKey();
+        }
+    }
+}
